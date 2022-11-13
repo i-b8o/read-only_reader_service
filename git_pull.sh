8df8b20aa52b4980f5676fed1a9711e8a230e290
i-b8o <blackview1617@gmail.com>
2022-11-13 20:48:56 +0300

    message

--- git_pull.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "read only pull"
-git pull origin main
--- git_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "supreme push"
-git add . && \
-git add -u && \
-git commit -m $1 && \
-git push origin master
